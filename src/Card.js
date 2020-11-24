eb536441b3b5d9b5ba4fcb5d838e2acbd81748e1
arielmoatti <github@arielmoatti.com>
2020-11-24 19:45:14 +0100

    card design and logic done

--- src/Card.js
+++ src/Card.js
@@ -61,7 +61,7 @@ export default function Card({ plant }) {
                     <div className="frontInfo">
                         <div className="plant-type">
                             <img
-                                id="card-icon"
+                                id="card-front-icon"
                                 className={
                                     plant.type == "Foliage"
                                         ? "icon-highlight"
@@ -70,7 +70,7 @@ export default function Card({ plant }) {
                                 src="/assets/foliage.png"
                             />
                             <img
-                                id="card-icon"
+                                id="card-front-icon"
                                 className={
                                     plant.type == "Flowering"
                                         ? "icon-highlight"
@@ -79,7 +79,7 @@ export default function Card({ plant }) {
                                 src="/assets/flowering.png"
                             />
                             <img
-                                id="card-icon"
+                                id="card-front-icon"
                                 className={
                                     plant.type == "Succulents"
                                         ? "icon-highlight"
@@ -88,7 +88,7 @@ export default function Card({ plant }) {
                                 src="/assets/succulents.png"
                             />
                             <img
-                                id="card-icon"
+                                id="card-front-icon"
                                 className={
                                     plant.type == "Edible"
                                         ? "icon-highlight"
@@ -100,13 +100,13 @@ export default function Card({ plant }) {
                         <div className="pet-air">
                             {plant.pet_safe && (
                                 <img
-                                    id="card-icon"
+                                    id="card-front-icon"
                                     src="/assets/pet_safe.png"
                                 />
                             )}
                             {plant.air_purifier && (
                                 <img
-                                    id="card-icon"
+                                    id="card-front-icon"
                                     src="/assets/air_purifier.png"
                                 />
                             )}
@@ -119,12 +119,70 @@ export default function Card({ plant }) {
                 className={plant.flipped ? undefined : "flagHidden"}
                 onClick={() => toggleCardSide(plant)}
             >
-                <p>watering {plant.watering}</p>
-                <p>light {plant.light}</p>
-                <p>{plant.indoor ? "indoor" : "outdoor"}</p>
-                <p>temp range {plant.temp_range}</p>
-                <p>max growth {plant.max_growth}</p>
-                <p>difficulty {plant.difficulty}</p>
+                <div id="back-card-top">
+                    <div className="img-container">
+                        <img
+                            src={plant.img_url || "/fallback-plant.png"}
+                            onError={(e) => {
+                                e.target.onerror = null;
+                                e.target.src = "/fallback-plant.png";
+                            }}
+                            alt={`${plant.common_name}`}
+                        />
+                    </div>
+                    <h3>{plant.common_name}</h3>
+                </div>
+                <div className="grid-container">
+                    <img
+                        id="card-back-icon"
+                        src={
+                            plant.watering == 1
+                                ? "/assets/watering_1.png"
+                                : plant.watering == 2
+                                ? "/assets/watering_2.png"
+                                : "/assets/watering_3.png"
+                        }
+                    />
+                    <img
+                        id="card-back-icon"
+                        src={
+                            plant.light == 1
+                                ? "/assets/light_1.png"
+                                : plant.light == 2
+                                ? "/assets/light_2.png"
+                                : "/assets/light_3.png"
+                        }
+                    />
+                    <img
+                        id="card-back-icon"
+                        src={
+                            plant.indoor
+                                ? "/assets/indoor.png"
+                                : "/assets/outdoor.png"
+                        }
+                    />
+
+                    <div className="temp-cont">
+                        <img id="card-back-icon" src="/assets/temp.png" />
+                        <p>{plant.temp_range} &#176;C</p>
+                    </div>
+                    <div className="growth-cont">
+                        <img id="card-back-icon" src="/assets/max_growth.png" />
+                        <p>{plant.max_growth} cm</p>
+                    </div>
+                    <div className="diff-cont">
+                        <img
+                            id="card-back-icon"
+                            src={
+                                plant.difficulty == 1
+                                    ? "/assets/difficulty_1.png"
+                                    : plant.difficulty == 2
+                                    ? "/assets/difficulty_2.png"
+                                    : "/assets/difficulty_3.png"
+                            }
+                        />
+                    </div>
+                </div>
             </div>
         </div>
     );
